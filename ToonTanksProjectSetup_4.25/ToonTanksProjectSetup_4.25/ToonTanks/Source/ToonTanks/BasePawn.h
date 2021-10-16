95b095696f06c768586d6eca714a9e3623369f70
Jakub Michalewicz <jakubmichalewicz1996@gmail.com>
2021-10-17 00:02:35 +0200

    .

--- ToonTanksProjectSetup_4.25/ToonTanksProjectSetup_4.25/ToonTanks/Source/ToonTanks/BasePawn.h
+++ ToonTanksProjectSetup_4.25/ToonTanksProjectSetup_4.25/ToonTanks/Source/ToonTanks/BasePawn.h
@@ -19,6 +19,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	class UCapsuleComponent* CapsuleComp;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
